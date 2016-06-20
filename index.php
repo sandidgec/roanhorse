13b6ce873a105e48b4f001dc5b4c25df529a8a17
Charles Sandidge <sandidgec@gmail.com>
2016-06-20 13:10:58 -0600

    first commit , added lots of stuff

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php require_once("lib/headutls.php");?>
+</head>
+
+<body class="jumbotron">
+<h3 class="col-md-6 col-md-offset-3 leadtag">RoanhorseConsulting.com coming May 2016</h3>
+<h3 class="col-md-6 col-md-offset-3 leadtag">@vrroanhorse<span class="fa fa-twitter" aria-hidden="true"></span></h3>
+
+</body>
+</html>
---
+++ lib/headutls.php
@@ -0,0 +1,23 @@
+<title>Roanhorse Consulting</title>
+
+<meta name="google-site-verification" content="sHVoWbCG1B8zLjSk1Xf8dYaW2ECIspaMD7ae_6Ec2Kk" />
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css"
+      integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7"
+      crossorigin="anonymous">
+
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css"
+      integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"
+        integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS"
+        crossorigin="anonymous"></script>
+
+<!-- Custom CSS -->
+<link rel="stylesheet" href="lib/css/style.css">
+
+<!-- Font Awesome -->
+<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+
