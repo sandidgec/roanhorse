2ba3fecf71f988f962a63015826f866a240e522e
Jeremiah Johnson <jeremiahsir@gmail.com>
2016-06-20 13:52:48 -0600

    added to some files

--- index.php
+++ index.php
@@ -3,10 +3,15 @@
 <head>
     <?php require_once("lib/headutls.php");?>
 </head>
+<body>
+<?php require_once("navbar.php");?>
 
-<body class="jumbotron">
-<h3 class="col-md-6 col-md-offset-3 leadtag">RoanhorseConsulting.com coming May 2016</h3>
-<h3 class="col-md-6 col-md-offset-3 leadtag">@vrroanhorse<span class="fa fa-twitter" aria-hidden="true"></span></h3>
+<div class="jumbotron">
+    <div class="container">
+        <h3 class="col-md-6 col-md-offset-3 leadtag">RoanhorseConsulting.com coming May 2016</h3>
+        <h3 class="col-md-6 col-md-offset-3 leadtag">@vrroanhorse<span class="fa fa-twitter" aria-hidden="true"></span></h3>
+    </div>
+</div>
 
 </body>
 </html>
---
+++ project.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php require_once("lib/headutls.php");?>
+</head>
+<body>
+<?php require_once("navbar.php");?>
+
+
+
+</body>
+</html>
